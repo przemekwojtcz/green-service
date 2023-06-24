4e11683621bb21aa1615869c90af55d205c4b1f3
przemekwojtcz <przemekwojtcz@gmail.com>
2023-06-24 15:46:42 +0200

    WIP

--- src/main/java/com/pwoj/service_green/test/StandardRedServiceClient.java
+++ src/main/java/com/pwoj/service_green/test/StandardRedServiceClient.java
@@ -1,15 +1,19 @@
 package com.pwoj.service_green.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StandardRedServiceClient implements RedServiceClient{
+
+    @Value(value = "${redService.baseUrl}")
+    private String baseUrl;
     @Override
     public RedServiceResponse test() {
         RestTemplate restTemplate = new RestTemplate();
         String url
-                = "http://localhost:8082/test";
+                = baseUrl +"/test";
         var response
                 = restTemplate.getForEntity(url, RedServiceResponse.class);
         return response.getBody();
