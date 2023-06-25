765893564884e7f8eca7ad0a5e630357a4f502e0
przemekwojtcz <przemekwojtcz@gmail.com>
2023-06-25 18:18:19 +0200

    WIP

--- src/main/java/com/pwoj/service_green/test/StandardRedServiceClient.java
+++ src/main/java/com/pwoj/service_green/test/StandardRedServiceClient.java
@@ -1,11 +1,15 @@
 package com.pwoj.service_green.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StandardRedServiceClient implements RedServiceClient{
+    Logger logger = LoggerFactory.getLogger(StandardRedServiceClient.class);
+
 
     @Value(value = "${redService.baseUrl}")
     private String baseUrl;
@@ -14,6 +18,7 @@ public class StandardRedServiceClient implements RedServiceClient{
         RestTemplate restTemplate = new RestTemplate();
         String url
                 = baseUrl +"/test";
+        logger.info("Red service url: " + baseUrl);
         var response
                 = restTemplate.getForEntity(url, RedServiceResponse.class);
         return response.getBody();
